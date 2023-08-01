d5548405c74ec6493c91c5715fad1caef1647fb4
Mateo Díaz <mateo.diaz@epn.edu.ec>
2023-08-01 08:46:51 -0500

    Login Firebase

--- 01-Android/app/src/main/java/mateoapp/com/appmoviles01/HFirebaseUIAuth.kt
+++ 01-Android/app/src/main/java/mateoapp/com/appmoviles01/HFirebaseUIAuth.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
@@ -32,9 +33,30 @@ class HFirebaseUIAuth : AppCompatActivity() {
         }
     }
 
-    fun registrarUsuariosPorPrimeraVez()
+    fun registrarUsuariosPorPrimeraVez(usuario: IdpResponse){/* usuario.email; usuario.phoneNumber; usuario.user.name;*/ }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hfirebase_uiauth)
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        btnLogin.setOnClickListener {
+            val providers = arrayListOf(
+                //Arreglo de PROVIDERS para logearse
+                //EJ: Correo, Facebook, Twitter
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+            //Construimos el intent de login
+            val signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
+            //Respuesta del intent Login
+            respuestaLoginAuthUi.launch(signInIntent)
+        }
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.setOnClickListener { seDeslogeo() }
+    }
+
+    fun seDeslogeo(){
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.visibility = View.INVISIBLE
+        btnLogin.visibility = View.VISIBLE
     }
 }
